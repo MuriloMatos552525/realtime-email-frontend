ecc281ab6c99f7f249828ef69f7520a81adf45c0
MuriloMatos552525 <murilocarlosm7@gmail.com>
2024-11-30 13:10:38 -0300

    aaaa

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,2 @@
-// utils/config.js
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
-export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || 'ws://localhost:8000';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://teste-balzani.azurewebsites.net';
+export const WS_URL = process.env.NEXT_PUBLIC_WS_URL || 'wss://teste-balzani.azurewebsites.net';
